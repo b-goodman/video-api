f25553abf6496b4402bbd8812142dea5f8a5e7f8
Ben Goodman <bgoodman@wolfram.com>
2019-10-10 23:41:34 -0400

    ignoring dist

--- dist/services/search/handlers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../../models/index");
-exports.searchVideos = (query) => {
-    return index_1.models.Video.textSearch(query);
-};
-//# sourceMappingURL=handlers.js.map
